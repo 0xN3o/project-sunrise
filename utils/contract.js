7467ab0d5026889fdab910a7514a99b714670f3e
carrotn0se <carrotnose@proton.me>
2023-01-27 21:50:58 +0800

    Create contract.js to connect with blockchain

---
+++ utils/contract.js
@@ -0,0 +1,46 @@
+export async function get_tree(contract, target, user) {
+  let parent;
+  await contract.methods
+    .parentOfToken(target)
+    .call()
+    .then((tokenId) => {
+      parent = tokenId;
+    });
+  const colors = {
+    [target]: "#aaa",
+    [user]: "#3f3",
+  };
+  return await get_children(parent, contract, colors);
+}
+
+async function get_children(root, contract, colors) {
+  let nfts = [];
+  let connections = [];
+
+  const max_depth = 3;
+  let queue = [[root, 0]];
+  while (queue.length > 0) {
+    const [parent, cur_depth] = queue.shift();
+    nfts.push({
+      id: parent,
+      label: parent,
+      color: colors[parent],
+    });
+    if (cur_depth < max_depth) {
+      let children;
+      await contract.methods
+        .referralTree(parent)
+        .call()
+        .then((tokenId) => {
+          children = tokenId;
+        });
+
+      children.forEach((child) => {
+        queue.push([child, cur_depth + 1]);
+        connections.push({ from: parent, to: child });
+      });
+    }
+  }
+
+  return { connections: connections, nfts: nfts };
+}
