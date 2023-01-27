9ee7c1240d427361e46668266250192490310324
carrotn0se <carrotnose@proton.me>
2023-01-27 21:51:05 +0800

    Use dynamic link

--- pages/tree/[userTokenId].js
+++ pages/tree/[userTokenId].js
@@ -2,14 +2,17 @@ import Navbar from "../../components/navbar/navbar";
 import PageHead from "../../components/pagehead";
 import Footer from "../../components/footer/footer";
 import BinaryTree from "../../components/tree/graph";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const { userTokenId } = router.query;
   return (
     <div>
       <PageHead title="Home" />
       <main>
         <Navbar activeKey="home" />
-        <BinaryTree target={100005} user={100006} />
+        <BinaryTree target={userTokenId} user={userTokenId} />
       </main>
       <Footer />
     </div>
